ef532954e6c3bba7f2011a1d37e577ae95a0e4e5
Filipe <filipevalles@gmail.com>
2025-06-06 14:57:13 -0300

    first commit

---
+++ src/main/java/com/l2/empacotador/EmpacotadorApplication.java
@@ -0,0 +1,13 @@
+package com.l2.empacotador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpacotadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpacotadorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/l2/empacotador/controller/PedidoController.java
@@ -0,0 +1,13 @@
+package com.l2.empacotador.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PedidoController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "API do Empacotador está no ar 🚀";
+    }
+}

e09873c61d2d5ca6a7ac89993931945e8de92fb9
Filipe <filipevalles@gmail.com>
2025-06-09 16:54:54 -0300

    Salva os dados do Usuário no banco de dados

--- src/main/java/com/filipeassaf/todolist/user/UserController.java
+++ src/main/java/com/filipeassaf/todolist/user/UserController.java
@@ -1,23 +1,21 @@
 package com.filipeassaf.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-     public void create(@RequestBody UserModel userModel){
-        System.out.println("Nome: " + userModel.getName());
-        System.out.println("Username: " + userModel.getUsername());
-        System.out.println("Senha: " + userModel.getPassword());
+     public UserModel create(@RequestBody UserModel userModel){
+        return this.userRepository.save(userModel);
     }
     
 }

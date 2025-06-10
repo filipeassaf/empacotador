3e886f2df6838f2cad7c726571f9ffb472c1c962
Filipe <filipevalles@gmail.com>
2025-06-10 15:20:28 -0300

    Valida Usuário cadastrado no banco de dados

--- src/main/java/com/filipeassaf/todolist/user/UserController.java
+++ src/main/java/com/filipeassaf/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package com.filipeassaf.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +17,15 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-     public UserModel create(@RequestBody UserModel userModel){
-        return this.userRepository.save(userModel);
+     public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }

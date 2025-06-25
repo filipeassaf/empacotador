85d2a979367c055fcc23e687e037dcdc3cc8dd91
Filipe <filipevalles@gmail.com>
2025-06-25 12:03:13 -0300

    Adiciona hash na senha do Usuário

--- src/main/java/com/filipeassaf/todolist/user/UserController.java
+++ src/main/java/com/filipeassaf/todolist/user/UserController.java
@@ -7,7 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +27,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado");
         }
+
+        var password = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(password);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
